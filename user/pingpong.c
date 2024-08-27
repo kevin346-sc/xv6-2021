fe26fcfba452fd2411e8d12228dcc40bdebf2085
kevin346-sc <18kkhuang2@stu.edu.cn>
2024-08-27 16:41:10 +0800

    fix pingpong bug

--- user/pingpong.c
+++ user/pingpong.c
@@ -17,29 +17,41 @@ main(int argc, char *argv[])
     pipe(p_c);
     pipe(c_p);
     char buf = 'a';
-    if(fork() == 0) // child
+    int pid = fork();
+    if(pid < 0)
     {
-        close(p_c[1]); // 关闭父写
-        close(c_p[0]); // 关闭父读
+        fprintf(2, "fork() error!\n");
+        close(p_c[WR]);
+        close(p_c[RD]);
+        close(c_p[WR]);
+        close(c_p[RD]);
+        exit(-1);
+    }
+    else if(pid == 0) // child
+    {
+        close(p_c[WR]); // 关闭父写
+        close(c_p[RD]); // 关闭父读
         
-        read(p_c[0], &buf, sizeof(char) );
+        read(p_c[RD], &buf, sizeof(char) );
         fprintf(1, "%d: received ping\n", getpid());
-        write(c_p[1], &buf, 1);
+        write(c_p[WR], &buf, 1);
 
-        close(p_c[0]);
-        close(c_p[1]);
+        close(p_c[RD]);
+        close(c_p[WR]);
+        exit(0);
     }
     else
     {
-        close(p_c[0]);
-        close(c_p[1]);
+        close(p_c[RD]);
+        close(c_p[WR]);
 
-        write(p_c[1], &buf, sizeof(char));
+        write(p_c[WR], &buf, sizeof(char));
+        read(c_p[RD], &buf, sizeof(char));
         fprintf(1, "%d: received pong\n", getpid());
-        read(c_p[0], &buf, sizeof(char));
 
-        close(p_c[1]);
-        close(c_p[0]);
+        close(p_c[WR]);
+        close(c_p[RD]);
+        exit(0);
     }
     exit(0);
 }
