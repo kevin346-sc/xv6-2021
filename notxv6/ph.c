c74cc3e3c0dc82cc4f4739638c0392b6b2e7c0f9
kevin346-sc <18kkhuang2@stu.edu.cn>
2024-09-15 15:57:18 +0800

    finish using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t locks[NBUCKET] = {PTHREAD_MUTEX_INITIALIZER};
 
 double
 now()
@@ -52,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&locks[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&locks[i]);
   }
 
 }
